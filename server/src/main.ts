e2dc67d6269d0b135487f2d2f5e23745a499307b
yaodieudonnesingo60 <yaodieudonnesingo6@gmail.com>
2026-08-24 16:55:28 +0000

    fix: pass custom logger to NestFactory

--- server/src/main.ts
+++ server/src/main.ts
@@ -53,7 +53,7 @@ function printStartupBanner(env: string, port: number, nodeVersion: string) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+    logger: new LoggerService(),
   });
   const customLogger = app.get(LoggerService);
   app.useLogger(customLogger);
